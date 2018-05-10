f5bbc113b50f94a0f853d6b220fed0e5251baaf4
jfdelgad <36968968+jfdelgad@users.noreply.github.com>
2018-05-10 17:44:15 +0200

    Add files via upload

---
+++ Example/clientExample.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.23; 
+
+import "./ChronosClients.sol";
+
+contract YourContract is Client {
+    address public chronosAddress;
+    address public admin; 
+    uint256 public counter;
+
+    constructor(address serviceAddress) public {
+        admin = msg.sender;
+        chronosAddress = serviceAddress;
+    }
+
+
+
+    function callBack() public {
+        counter++;
+        setCallrequest(block.number + 5, 200000, chronosAddress);
+    }
+
+    
+    function () public payable {}
+    function kill() public {
+        selfdestruct(admin);
+    }
+
+}
