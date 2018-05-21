ab3560395ee0f1dd668ae54082aa9a0660364f64
jfdelgad <36968968+jfdelgad@users.noreply.github.com>
2018-05-21 18:21:39 +0200

    Create ethTempusClientRinkeby.sol

---
+++ EthTempus/ethTempusClientRinkeby.sol
@@ -0,0 +1,63 @@
+pragma solidity ^0.4.23; 
+
+
+/******************************************************************************************************* */
+library SafeMath {
+  /** @dev Multiplies two numbers, throws on overflow.*/
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {return 0;}
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
+    }
+
+  /** @dev Integer division of two numbers, truncating the quotient.*/
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a / b;
+        return c;
+    }
+
+  /**@dev Substracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).*/
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+  /** @dev Adds two numbers, throws on overflow.*/
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+
+}
+
+
+contract EthTempus {
+    uint256 public serviceFee;
+    function registerCall(uint256 callOnBlock, uint256 maxGasValue, uint256 callId) public;
+
+
+}
+
+
+contract EthTempusClient {
+    
+    address public ethTempusAddress;
+    EthTempus ethTempusInstance;
+    
+    constructor() public {
+        ethTempusAddress = 0x3e82a82f018a8ebf007c3805f467164dc11f47ac;
+        ethTempusInstance = EthTempus(ethTempusAddress);
+    }
+    function setCallrequest(uint256 blockNumber, uint256 maxGasValue, uint256 callId) public {
+        ethTempusInstance.registerCall(blockNumber, maxGasValue, callId);
+    }
+
+    function pay(uint256 callCost) public {
+        require(msg.sender == ethTempusAddress);
+        msg.sender.transfer(callCost);
+    }
+
+    function callBack(uint callId) public;
+}
